36dc4b8ec797bf7dbaa437a8374da3019fd4f326
Ruth Newman <ruthnewman@gmail.com>
2019-05-19 21:24:59 +0100

    added placeholder commands for list and read

--- app.js
+++ app.js
@@ -2,18 +2,39 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const getThoughts = require('./thoughts.js');
 
-const command = process.argv[2];
-console.log(process.argv);
-console.log(yargs.argv);
+yargs.version('1.1.0')
 
-switch(command) {
-  case 'add':
-    console.log('Adding a thought');
-    break;
-  case 'remove':
-    console.log('Removing a thought');
-    break;
-  default:
-    console.log('Try again');
-    break;
-}
+yargs.command({
+  command: 'add',
+  describe: 'Add a new thought',
+  handler: function() {
+    console.log('Adding a new thought')
+  }
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a thought from the list',
+  handler: function() {
+    console.log('Removing that thought from your list')
+  }
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'Listing all thoughts',
+  handler: function() {
+    console.log('Listing out all your thoughts')
+  }
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Reading a thought',
+  handler: function() {
+    console.log('Reading a thought')
+  }
+})
+
+
+console.log(yargs.argv);
